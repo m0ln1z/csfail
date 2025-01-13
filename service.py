7de52ea87197cba3036a2c84f01db10cdcffba93
m0ln1z <anartur476@gmail.com>
2025-01-13 21:33:02 +0500

    add: увеличил лимит триггера

--- service.py
+++ service.py
@@ -40,7 +40,8 @@ def fetchSpinValue():
     chromeOptions.add_argument("--no-sandbox")
     chromeOptions.add_argument("--disable-dev-shm-usage")
     chromeOptions.add_argument("--disable-blink-features=AutomationControlled")
-    chromeOptions.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chromeOptions.add_experimental_option(
+        "excludeSwitches", ["enable-automation"])
     chromeOptions.add_experimental_option("useAutomationExtension", False)
     chromeOptions.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
@@ -53,40 +54,41 @@ def fetchSpinValue():
         print("Ожидание загрузки страницы...")
 
         # Ожидание появления элемента с увеличенным временем
-        element = WebDriverWait(driver, 60).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, ".rounds-stats__color.rounds-stats__color_20x"))
-        )
-        
+        element = WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, ".rounds-stats__color.rounds-stats__color_20x")))
+
         # Если элемент найден, выводим его HTML для проверки
         print("Элемент найден:")
         print(element.get_attribute("outerHTML"))
-        
+
         spinValue = element.text.strip()
         print(f"Забираем значение спина: {spinValue}")
-        
+
         # Преобразуем в целое число, если удается
         try:
             return int(spinValue)
         except ValueError:
-            print(f"Неверное значение спина: {spinValue}. Не могу преобразовать в тип integer.")
+            print(
+                f"Неверное значение спина: {spinValue}. Не могу преобразовать в тип integer.")
             return None
 
     except Exception as e:
         print(f"Ошибка получения данных со спинов: {e}")
-        
+
         # Сохраняем HTML и скриншот для отладки
         with open("page_source.html", "w", encoding="utf-8") as file:
             file.write(driver.page_source)
         driver.save_screenshot("screenshot.png")
         print("HTML страницы и скриншот сохранены для анализа.")
-        
+
         return None
 
     finally:
         driver.quit()
 
 
-lastSentSpinValue = None  
+lastSentSpinValue = None
+
 
 async def checkConditionsAndNotify():
     """Функция проверки условий и отправки уведомлений."""
@@ -102,11 +104,14 @@ async def checkConditionsAndNotify():
     if len(spinHistory) > 100:
         spinHistory.pop(0)
 
-    print(f"Обновление истории спинов: {spinHistory[-10:]}")  # Отладочная информация
+    # Отладочная информация
+    print(f"Обновление истории спинов: {spinHistory[-10:]}")
 
-    # Если новое значение спина совпадает с последним отправленным, ничего не делаем
+    # Если новое значение спина совпадает с последним отправленным, ничего не
+    # делаем
     if spinValue == lastSentSpinValue:
-        print(f"Значение золотой {spinValue} не изменено. Пропускаем уведомление.")
+        print(
+            f"Значение золотой {spinValue} не изменено. Пропускаем уведомление.")
         return
 
     # Формируем сообщение в зависимости от значения спина
@@ -122,6 +127,16 @@ async def checkConditionsAndNotify():
         message = "4 золотых за последние 100 спинов"
     elif spinValue == 5:
         message = "5 золотых за последние 100 спинов"
+    elif spinValue == 6:
+        message = "6 золотых за последние 100 спинов"
+    elif spinValue == 7:
+        message = "7 золотых за последние 100 спинов"
+    elif spinValue == 8:
+        message = "8 золотых за последние 100 спинов"
+    elif spinValue == 9:
+        message = "9 золотых за последние 100 спинов"
+    elif spinValue == 10:
+        message = "10 золотых за последние 100 спинов"
     elif spinValue == 35:
         if 35 not in spinHistory[-100:]:
             message = "0 золотых за последние 100 спинов"
@@ -132,8 +147,9 @@ async def checkConditionsAndNotify():
             if spinsSinceLast35 >= 85:
                 message = f"С последней золотой 35х прошло {spinsSinceLast35} спинов"
             else:
-                print(f"С момента последнего 35 прошло {spinsSinceLast35} спинов. Уведомление не отправлено.")
-                return  
+                print(
+                    fп
+                return
     else:
         print(f"Неизвестное значение: {spinValue}. Пропуск.")
         return
@@ -148,7 +164,8 @@ async def sendNotification(message):
     """Функция для отправки уведомлений в Telegram."""
     for _ in range(3):  # Отправляем сообщение 3 раза
         await bot.send_message(chatId, message)
-        await asyncio.sleep(5) # Delay для отправки 3 сообщений
+        await asyncio.sleep(5)  # Delay для отправки 3 сообщений
+
 
 async def main():
     """Основной цикл программы."""
